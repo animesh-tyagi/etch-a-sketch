58954b2c3c0c8326260d427a31b6e79cfee08684
animesh-tyagi <animesh.tyagi.720@gmail.com>
2025-04-05 14:05:59 +0000

    reduced board size

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ btn.addEventListener("click", () => changeGrid());
 
 function changeGrid() {
     let res = prompt("Enter the number of boxes(1-100): ");
+    if (res === null || "") {
+        return;
+    }
+
     if (res < 0 || res >100) {
         alert("Please enter a number between 1-100");
         return;
@@ -19,8 +23,8 @@ function createBoxes(res) {
     for(let i=0; i<res*res; i++) {
         let box = document.createElement("div");
         box.style.boxSizing = "border-box";
-        box.style.height = `${960/res}px`;
-        box.style.width = `${960/res}px`;
+        box.style.height = `${500/res}px`;
+        box.style.width = `${500/res}px`;
         box.style.display = "flex";
         box.style.border = "1px solid black";
 
